46d8b61dd60f9791c540be3fe708827379f16363
Mostafa Abbasi <abbasi@uvic.ca>
2026-02-09 04:51:58 +0000

    Edit preload_cache.sh

--- scripts/preload_cache.sh
+++ scripts/preload_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Preloading Redis cache..."
 for i in $(seq 0 9999); do
-  redis-cli SET "user:$i" "value_$i" EX 60 > /dev/null
+  docker exec -it redis redis-cli SET "user:$i" "value_$i" EX 60 > /dev/null
 done
 echo "Done."
